a4f7849402bae28465ad2ab3846a891f812a2c31
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-11 08:40:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -21,7 +21,7 @@ def speak_to_grandma(phrase)
   # phrase = 'WHAT DID YOU EAT TODAY'
   # what is expected return "NO, NOT SINCE 1938!"
   # is what we are saying (phrase) all uppercase?
-  elsif phrase
+  elsif phrase ==
     return "NO, NOT SINCE 1938!"
 
   
